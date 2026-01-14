5178e54cd48173f1fcf4f2d38a8a14c84198f4bc
root <root@ip-172-31-26-56.ec2.internal>
2026-01-14 13:38:47 +0000

    Fix: separate index.tsx and useBoard hook

--- src/components/Board/hooks/useBoard.ts
+++ src/components/Board/hooks/useBoard.ts
@@ -1,17 +1,11 @@
 import { useContext } from "react";
-import { BoardContext } from "../context/BoardContext";
+import { BoardContext } from "../../../context/BoardContext";
 
 /**
- * Returns the properties of the board such as tile container width or tile count.
+ * Returns board-related values like container width and tile count
  */
 export const useBoard = (): [number, number] => {
-  const context = useContext(BoardContext);
-
-  if (!context) {
-    throw new Error("useBoard must be used within a BoardProvider");
-  }
-
-  const { containerWidth, tileCount } = context;
+  const { containerWidth, tileCount } = useContext(BoardContext);
   return [containerWidth, tileCount];
 };
 
